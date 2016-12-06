875c305a2c6855644d5dcb0c52361a1825d33a1b
Alexander Kohonovsky <saniaky@gmail.com>
2016-12-06 16:21:21 +0300

    Refactoring

--- src/main/java/com/saniaky/Parser.java
+++ src/main/java/com/saniaky/Parser.java
@@ -2,26 +2,21 @@ package com.saniaky;
 
 import de.jetwick.snacktory.HtmlFetcher;
 import de.jetwick.snacktory.JResult;
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.util.Units;
-import org.apache.poi.xwpf.usermodel.BreakType;
-import org.apache.poi.xwpf.usermodel.Document;
-import org.apache.poi.xwpf.usermodel.ParagraphAlignment;
-import org.apache.poi.xwpf.usermodel.XWPFDocument;
-import org.apache.poi.xwpf.usermodel.XWPFParagraph;
-import org.apache.poi.xwpf.usermodel.XWPFRun;
+import org.apache.poi.xwpf.usermodel.*;
 
 import javax.imageio.ImageIO;
+import javax.imageio.ImageReader;
+import javax.imageio.stream.ImageInputStream;
 import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.net.URL;
 import java.nio.file.Files;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -33,10 +28,11 @@ public class Parser {
     private static final String URLS_TXT = "urls.txt";
     private static final int ARTICLES_PER_FILE = 100;
 
-
     private static final int IMAGE_MAX_WIDTH_PX = 200;
-    private static final int resolveTimeout = 30000;
+    private static final int RESOLVE_TIMEOUT = 30000;
     private static final String TITLE_STYLE = "Title";
+    private static final String TEMPLATE_DOCX = "template.docx";
+    private static final String TEMP_FILE = "targetFile.tmp";
 
     public static void main(String[] args) throws IOException, InvalidFormatException {
         Parser parser = new Parser();
@@ -47,21 +43,16 @@ public class Parser {
         int fileNumber = 1;
         int articlesNumber = 0;
 
-        for (int row = 0; row < urls.size(); row++) {
-            JResult res;
+        for (String url : urls) {
             try {
-                res = fetcher.fetchAndExtract(urls.get(row), resolveTimeout, true);
+                JResult result = fetcher.fetchAndExtract(url, RESOLVE_TIMEOUT, true);
+                parser.addArticle(document, result);
             } catch (Exception e) {
-                System.out.println("Не получается загрузить статью: " + urls.get(row));
+                System.out.println("Не получается загрузить статью: " + url);
                 System.out.println("Причина: " + e.getMessage());
                 continue;
             }
 
-            String text = res.getText();
-            String title = res.getTitle();
-            String imageUrl = res.getImageUrl();
-            System.out.println("Строка " + (row + 1) + ": " + title);
-            parser.addArticle(document, title, text, imageUrl);
             articlesNumber++;
 
             if (articlesNumber % ARTICLES_PER_FILE == 0) {
@@ -82,25 +73,41 @@ public class Parser {
     }
 
     private static XWPFDocument createDocument() throws IOException {
-        return new XWPFDocument(new FileInputStream("template.docx"));
+        return new XWPFDocument(new FileInputStream(TEMPLATE_DOCX));
     }
 
-    private void addArticle(XWPFDocument document, String articleTitle, String articleText, String imageUrl)
-            throws IOException, InvalidFormatException {
+    private void addArticle(XWPFDocument document, JResult article) {
+        addTitle(document, article);
+        addImageIfExist(document, article);
+        addText(document, article);
+        System.out.println(article.getTitle());
+    }
 
-        // Add title
+    private void addText(XWPFDocument document, JResult article) {
+        XWPFParagraph paragraph = document.createParagraph();
+        XWPFRun textRun = paragraph.createRun();
+        textRun.setText(article.getText());
+        paragraph.setAlignment(ParagraphAlignment.BOTH);
+        textRun.addBreak(BreakType.PAGE);
+    }
+
+    private void addTitle(XWPFDocument document, JResult article) {
         XWPFParagraph paragraph = document.createParagraph();
         paragraph.setStyle(TITLE_STYLE);
         XWPFRun title = paragraph.createRun();
-        title.setText(articleTitle);
+        title.setText(article.getTitle());
+    }
 
-        // Add image if exist
+    private void addImageIfExist(XWPFDocument document, JResult article) {
+        String imageUrl = article.getImageUrl();
         if (StringUtils.isNotEmpty(imageUrl)) {
-            paragraph = document.createParagraph();
-            XWPFRun link = paragraph.createRun();
             try {
                 InputStream imageStream = new URL(imageUrl).openStream();
-                BufferedImage bimg = ImageIO.read(imageStream);
+                File targetFile = new File(TEMP_FILE);
+                FileUtils.copyInputStreamToFile(imageStream, targetFile);
+
+                // get image info
+                BufferedImage bimg = ImageIO.read(FileUtils.openInputStream(targetFile));
                 if (bimg != null) {
                     int width = bimg.getWidth();
                     int height = bimg.getHeight();
@@ -109,37 +116,68 @@ public class Parser {
                         width = IMAGE_MAX_WIDTH_PX;
                         height = (int) (ratio * width);
                     }
-                    width = Units.toEMU(width);
-                    height = Units.toEMU(height);
-                    imageStream = new URL(imageUrl).openStream(); // TODO saniaky: reuse stream
-                    link.addPicture(imageStream, Document.PICTURE_TYPE_JPEG, imageUrl, width, height);
+
+                    int pictureType = getPictureType(targetFile);
+                    if (pictureType != -1) {
+                        XWPFParagraph paragraph = document.createParagraph();
+                        XWPFRun link = paragraph.createRun();
+                        link.addPicture(
+                                FileUtils.openInputStream(targetFile),
+                                pictureType,
+                                imageUrl,
+                                Units.toEMU(width), Units.toEMU(height));
+                    }
                 }
-            } catch (IOException | ArrayIndexOutOfBoundsException e) {
+            } catch (IOException | ArrayIndexOutOfBoundsException | InvalidFormatException e) {
                 System.out.println("Не получается загрузить изображение: " + imageUrl);
                 System.out.println("Причина: " + e.getMessage());
             }
         }
-
-        // Article text
-        paragraph = document.createParagraph();
-        XWPFRun article = paragraph.createRun();
-        article.setText(articleText);
-        paragraph.setAlignment(ParagraphAlignment.BOTH);
-        article.addBreak(BreakType.PAGE);
     }
 
-    private List<String> getUrls() throws IOException {
-        URL resource = getUrl(URLS_TXT);
+    private int getPictureType(File targetFile) throws IOException {
+        String formatName = "";
+        ImageInputStream iis = ImageIO.createImageInputStream(targetFile);
+        Iterator<ImageReader> imageReaders = ImageIO.getImageReaders(iis);
+        while (imageReaders.hasNext()) {
+            ImageReader reader = imageReaders.next();
+            formatName = reader.getFormatName();
+        }
+
+        switch (formatName.toLowerCase()) {
+            case "jpg":
+                return Document.PICTURE_TYPE_JPEG;
+            case "jpeg":
+                return Document.PICTURE_TYPE_JPEG;
+            case "png":
+                return Document.PICTURE_TYPE_PNG;
+            case "gif":
+                return Document.PICTURE_TYPE_GIF;
+            case "bmp":
+                return Document.PICTURE_TYPE_BMP;
+            default:
+                return -1;
+        }
+    }
+
+    private List<String> getUrls() {
+        URL resource = getResourceUrl(URLS_TXT);
         if (resource == null) {
             System.out.println("Файл со ссылками не найден!");
             return Collections.emptyList();
         }
 
         String fileName = resource.getFile();
-        return Files.readAllLines(new File(fileName).toPath());
+        try {
+            return Files.readAllLines(new File(fileName).toPath());
+        } catch (IOException e) {
+            System.out.println("Не получается прочитать файл со ссылками - " + e.getMessage());
+        }
+
+        return Collections.emptyList();
     }
 
-    private URL getUrl(String fileName) {
+    private URL getResourceUrl(String fileName) {
         ClassLoader classLoader = getClass().getClassLoader();
         return classLoader.getResource(fileName);
     }
